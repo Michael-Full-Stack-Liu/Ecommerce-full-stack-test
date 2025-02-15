aa49e9abda66d491a85243abad9545e530874cb2
Michael-Full-Stack-Liu <liujianzhong8@hotmail.com>
2025-02-15 03:11:22 -0800

    save

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,18 @@ import AddToCart from "@/components/cartAction/addToCart";
 import RemoveFromYourCart from "@/components/cartAction/removeFromYourCart";
 import SalesCampaignBanner from "@/components/layout/salesCampaignBanner";
 import ProductGrid from "@/components/product/productGrid";
+import { Product } from "@/sanity.types";
 import { getAllProducts } from "@/sanity/lib/client";
 
 
 
 export default async function Home() {
 
-  const products = await getAllProducts();
+  const products: Product = await getAllProducts();
 
   return (
     <div>
-      
-      
-      
+
       <div className="flex flex-col w-full">
       {/* <SignUp /> */}
       <SalesCampaignBanner />
